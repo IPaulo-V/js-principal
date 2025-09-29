4cbea750b972fd65ca6ece71586e811b8446ad5f
IPaulo-V <paulo.victor_sv@proton.me>
2025-09-29 20:39:34 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Número secreto";
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = "Escolha um numero de 1 à 10";
+
+function verificarChute() {
+  console.log('Obotão foi precisonado')  
+}
