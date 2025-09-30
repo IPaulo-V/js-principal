118c0cde9029e2863d32344a57a06a713fba8875
IPaulo-V <paulo.victor_sv@proton.me>
2025-09-29 21:05:37 -0300

    aprimoramentos e testes

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Número secreto";
+let numeroSecreto = gereNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = "Escolha um numero de 1 à 10";
+function exibirTexto(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+exibirTexto('h1', 'Número secreto');
+exibirTexto('p', 'Escolha um numero de 1 à 10');
 
 function verificarChute() {
-  console.log('Obotão foi precisonado')  
+  console.log('O botão foi precisonado');  
+}
+
+function gereNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
 }
